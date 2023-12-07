8d4c55536c9dc5fe4a32d9f3debac5bf516e676a
yuzh mash <bondkyrylo@gmail.com>
2023-12-07 18:20:23 +0200

    Changed ports

--- comment_section/settings.py
+++ comment_section/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-z)039!799*okca-m-cvt9@x*folwzpmp@yxr@ypbbulf4e+fu_"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
